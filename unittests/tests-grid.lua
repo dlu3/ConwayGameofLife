0e1bdc54562f357fb2159dfbad1f3afb943eb19a
dlu3 <132450069+dlu3@users.noreply.github.com>
2023-10-09 23:30:13 +0800

    grid function name has changed

--- unittests/tests-grid.lua
+++ unittests/tests-grid.lua
@@ -2,18 +2,18 @@ module(..., package.seeall)
 
 -- Setup grid with blinker_v
 local grid = Grid:new(7, 7)
-grid:setCoordinate(4, 3, 1)
-grid:setCoordinate(4, 4, 1)
-grid:setCoordinate(4, 5, 1)
+grid:set_coordinate(4, 3, 1)
+grid:set_coordinate(4, 4, 1)
+grid:set_coordinate(4, 5, 1)
 
 function test_grid_exists()
     assert_true(grid)
 end
 
 function test_grid_blinker_exists()
-    assert_equal(grid:getCoordinate(4, 3), 1)
-    assert_equal(grid:getCoordinate(4, 4), 1)
-    assert_equal(grid:getCoordinate(4, 5), 1)
+    assert_equal(grid:get_coordinate(4, 3), 1)
+    assert_equal(grid:get_coordinate(4, 4), 1)
+    assert_equal(grid:get_coordinate(4, 5), 1)
 end
 
 -- assert neighbour functions
@@ -26,20 +26,20 @@ local function getSum(table)
 end
 
 function test_grid_neighbour_return()
-    assert_table(grid:getNeighbours(4, 4))
+    assert_table(grid:get_neighbours(4, 4))
 end
 
 function test_grid_neighbour_blinker()
-    assert_equal(getSum(grid:getNeighbours(4, 3)), 1)
-    assert_equal(getSum(grid:getNeighbours(4, 4)), 2)
-    assert_equal(getSum(grid:getNeighbours(4, 5)), 1)
+    assert_equal(getSum(grid:get_neighbours(4, 3)), 1)
+    assert_equal(getSum(grid:get_neighbours(4, 4)), 2)
+    assert_equal(getSum(grid:get_neighbours(4, 5)), 1)
 
-    assert_equal(getSum(grid:getNeighbours(3, 4)), 3)
-    assert_equal(getSum(grid:getNeighbours(5, 4)), 3)
+    assert_equal(getSum(grid:get_neighbours(3, 4)), 3)
+    assert_equal(getSum(grid:get_neighbours(5, 4)), 3)
 end
 
 function test_grid_neighbour_edge()
-    assert_equal(#grid:getNeighbours(1, 1), 8)
-    assert_equal(#grid:getNeighbours(2, 1), 8)
+    assert_equal(#grid:get_neighbours(1, 1), 0)
+    assert_equal(#grid:get_neighbours(2, 1), 0)
 end
 
