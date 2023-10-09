60c566f83f61bcbe8416f56f0700ab8a2fda895f
dlu3 <132450069+dlu3@users.noreply.github.com>
2023-10-09 23:29:03 +0800

    implement restart and grid tapping

--- scenes/grid-scene.lua
+++ scenes/grid-scene.lua
@@ -14,6 +14,7 @@ local native = require("native")
 local is_playing = false
 local minCellSize = 1
 local maxCellSize = 20
+local heightOffset = 100 -- Keep grid from extending into buttons
 
 local start = "Start"
 local stop = "Stop"
@@ -21,25 +22,26 @@ local stop = "Stop"
 local width = composer.getVariable( "gridSizeX" )
 local height = composer.getVariable( "gridSizeY" )
 local iterationSpeed = composer.getVariable( "iterationSpeed" )
+local do_random = composer.getVariable( "doRandom" )
 local randomSeed = composer.getVariable( "randomSeed" )
 
 -- clamp cell sizes
 local smallestCellSize = math.min(
     display.contentWidth / width, 
-    display.contentHeight / height )
+    (display.contentHeight - heightOffset) / height )
 local finalCellSize = (smallestCellSize > maxCellSize and maxCellSize) or 
                      (smallestCellSize < minCellSize and minCellSize) or
                      (smallestCellSize)
 
-local startGrid = Grid:new(width, height)
-local nextGrid = Grid:new(width, height)
+local startGrid = grid:new(width, height)
+local nextGrid = grid:new(width, height)
 
 local gridGroup = display.newGroup()
 local controlGroup = display.newGroup()  
 
---- Clears children from a DisplayObject
--- @param group group object
--- @param offset highest index to not be removed
+--- Clears children from a DisplayObject.
+-- @param group GroupObject: group object.
+-- @param offset int: highest index to not be removed.
 local function clear_children(group, offset)
     while group.numChildren > offset do
         local child = group[offset+1]
@@ -47,9 +49,9 @@ local function clear_children(group, offset)
     end
 end
 
---- Draws grid onto screen
--- @param gridObject the grid to be read from
--- @param displayGroup the display to draw grid into
+--- Draws grid onto screen.
+-- @param gridObject Grid: the grid to be read from.
+-- @param displayGroup GroupObject: the display to draw grid into.
 local function draw_grid(gridObject, displayGroup)
     local offset = finalCellSize / 2 -- account for central anchor position
     local gridArray = gridObject:get_grid()
@@ -69,26 +71,26 @@ local function draw_grid(gridObject, displayGroup)
     end
 end
 
---- Functions to iterate per cycle
--- @param grid the grid to be evolved
--- @param group the group to be drawn in
+--- Functions to iterate per cycle.
+-- @param grid Grid: the grid to be evolved.
+-- @param group GroupObject: the group to be drawn in.
 local function iterate(grid, group)
-    print("iterate")
     grid:set_evolution() 
     draw_grid(grid, group) 
 end
 
---- Encloses iterate for use in timer functions
+--- Encloses iterate for use in timer functions.
+-- Used because parameters can't be specified.
 local function iterateClosure()
     return iterate(nextGrid, gridGroup)
 end
 
---- Listener for evolution button
--- @param event tap event
+--- Listener for evolution button.
+-- @param event event: tap event.
 local function play_life(event)
     local button = event.target
     if is_playing == false then
-        timer.performWithDelay( iterationSpeed, iterateClosure, -1, "play" )
+        timer.performWithDelay( iterationSpeed, iterateClosure, -1, "play" ) -- pauses before iterating
         button:setLabel(stop)
         is_playing = true
     elseif is_playing == true then
@@ -98,6 +100,44 @@ local function play_life(event)
     end
 end
 
+--- Listener for restart button.
+-- Needs to be enclosed.
+-- @param event event: tap event.
+-- @param button ButtonWidget: button to be auto-paused.
+local function restart_life(event, button)
+    is_playing = false
+    timer.pause("play")
+    button:setLabel(start)
+
+    clear_children(gridGroup, 1)
+    nextGrid:copy_grid(startGrid:get_grid()) -- NOTE: do NOT set grid as another directly
+    draw_grid(nextGrid, gridGroup)
+end
+
+--- Allows user to tap cells alive or not.
+-- "Borrowed" from https://stackoverflow.com/a/27895373
+-- @param event event: listening when tapping on grid
+local function tap_grid(event)
+    if event.phase == "began" then
+        -- Clamp x and y values to within grid range
+        local x = math.min(finalCellSize * width, math.max(1, event.x - gridGroup.x))
+        local y = math.min(finalCellSize * height, math.max(1, event.y - gridGroup.y))
+        local xcoord = math.ceil(x / finalCellSize)
+        local ycoord = math.ceil(y / finalCellSize)
+        local cell = nextGrid:get_coordinate(xcoord, ycoord)
+
+        if cell == 1 then
+            nextGrid:set_coordinate(xcoord, ycoord, 0)
+        elseif cell == 0 then
+            nextGrid:set_coordinate(xcoord, ycoord, 1)
+        end
+
+        draw_grid(nextGrid, gridGroup)
+    end
+end
+
+gridGroup:addEventListener("touch", tap_grid)
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -106,10 +146,7 @@ end
 function scene:create( event )
 
     local sceneGroup = self.view
-    local params = event.params
-    
     -- Code here runs when the scene is first created but has not yet appeared on screen
-
     
     gridGroup.x = display.contentCenterX - (finalCellSize * width) * 0.5
     gridGroup.y = 0
@@ -122,10 +159,9 @@ function scene:create( event )
     background.strokeWidth = 3
     background:setFillColor(0)
     background:setStrokeColor(1, 0, 0)
-    gridGroup:insert(background)
 
-    if params.random then startGrid:set_all_random(randomSeed) end
-    nextGrid = startGrid
+    if do_random then startGrid:set_all_random(randomSeed) end
+    nextGrid:copy_grid(startGrid:get_grid()) -- NOTE: do NOT set a grid as another directly
     draw_grid(nextGrid, gridGroup)
 
     --- Button for starting game
@@ -156,40 +192,15 @@ function scene:create( event )
             -- Shape properties
             shape = "roundedRect",
             fillColor = { default = {1, 1, 1, 1}, over = {1, 1, 1, 0.7}},
-            onRelease = function()
-                timer.cancel( "play" )
-                clear_children(gridGroup, 1)
-                nextGrid = startGrid
-                draw_grid(startGrid, gridGroup)
-            end
+            onRelease = function(target) restart_life(target, buttonPlay) end
         }
     )
-
-    --- Button for creating new grid
-    local buttonNewGrid = widget.newButton( 
-        {
-            id = "button_StartEvolution",
-            label = "New Grid",
-            x = display.contentCenterX + 100,
-            y = display.contentCenterY + 200,
-            width = 80,
-            height = 40,
-            -- Shape properties
-            shape = "roundedRect",
-            fillColor = { default = {1, 1, 1, 1}, over = {1, 1, 1, 0.7}},
-            onRelease = function(event)
-                timer.cancel( "play" )
-                sceneGroup:removeSelf()
-                composer.gotoScene("scenes.config-scene")
-            end
-        }
-    )
-  
+    
+    gridGroup:insert(background)
     controlGroup:insert(buttonPlay)
     controlGroup:insert(buttonRestart)
-    controlGroup:insert(buttonNewGrid)
-    
-
+    sceneGroup:insert(gridGroup)
+    sceneGroup:insert(controlGroup)
 end
 
 -- show()
