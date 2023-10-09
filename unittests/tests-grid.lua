57ea6c847cac826a7669c2ebb775f1aa4877c413
dlu3 <132450069+dlu3@users.noreply.github.com>
2023-10-10 01:49:15 +0800

    additional tests

--- unittests/tests-grid.lua
+++ unittests/tests-grid.lua
@@ -1,22 +1,15 @@
 module(..., package.seeall)
+local grid = require("modules.grid")
 
--- Setup grid with blinker_v
-local grid = Grid:new(7, 7)
-grid:set_coordinate(4, 3, 1)
-grid:set_coordinate(4, 4, 1)
-grid:set_coordinate(4, 5, 1)
+-- Init values
+local width = lunatest.random_int(5, 200)
+local height = lunatest.random_int(5, 200)
 
-function test_grid_exists()
-    assert_true(grid)
-end
+--- Helper functions
 
-function test_grid_blinker_exists()
-    assert_equal(grid:get_coordinate(4, 3), 1)
-    assert_equal(grid:get_coordinate(4, 4), 1)
-    assert_equal(grid:get_coordinate(4, 5), 1)
-end
-
--- assert neighbour functions
+--- Sums integers in a table
+-- @param table table: array of values.
+-- @return sum int: sum of integers.
 local function getSum(table)
     local sum = 0
     for i=1, #table do
@@ -25,10 +18,38 @@ local function getSum(table)
     return sum
 end
 
-function test_grid_neighbour_return()
-    assert_table(grid:get_neighbours(4, 4))
+-- Setup grid with blinker_v
+local grid = grid:new(width, height)
+grid:set_coordinate(4, 3, 1)
+grid:set_coordinate(4, 4, 1)
+grid:set_coordinate(4, 5, 1)
+
+function test_grid_exists()
+    assert_true(grid)
 end
 
+-- Getters
+
+function test_grid_blinker_exists()
+    assert_equal(grid:get_coordinate(4, 3), 1)
+    assert_equal(grid:get_coordinate(4, 4), 1)
+    assert_equal(grid:get_coordinate(4, 5), 1)
+end
+
+function test_grid_width()
+    assert_equal(grid:get_width(), width)
+end
+
+function test_grid_height()
+    assert_equal(grid:get_height(), height)
+end
+
+-- Assert table return by getting its length
+function test_grid_neighbour_return()
+    assert_gte(1, #grid:get_neighbours(4, 4))
+end
+
+-- Assert sum of neighbours are correct
 function test_grid_neighbour_blinker()
     assert_equal(getSum(grid:get_neighbours(4, 3)), 1)
     assert_equal(getSum(grid:get_neighbours(4, 4)), 2)
@@ -39,7 +60,7 @@ function test_grid_neighbour_blinker()
 end
 
 function test_grid_neighbour_edge()
-    assert_equal(#grid:get_neighbours(1, 1), 0)
-    assert_equal(#grid:get_neighbours(2, 1), 0)
+    assert_equal(getSum(grid:get_neighbours(1, 1)), 0)
+    assert_equal(getSum(grid:get_neighbours(2, 1)), 0)
 end
 
