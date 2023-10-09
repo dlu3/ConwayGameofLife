ed5e775c2a28d25ca886020100ac4d40935ce753
dlu3 <132450069+dlu3@users.noreply.github.com>
2023-10-09 23:29:21 +0800

    pattern module isnt used anymore

--- unittests/Mytests.lua
+++ unittests/Mytests.lua
@@ -1,6 +1,6 @@
 print("--- Unit tests start here ---")
 
 lunatest.suite("unittests.tests-grid")
-lunatest.suite("unittests.tests-pattern")
+--lunatest.suite("unittests.tests-pattern")
 --lunatest.suite("unittests.tests-main")
 lunatest.run()
